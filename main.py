c107f9ca0b4a9171c1dc505355328b568f46472a
RodrigoAlban <rodrigoalban@alunos.utfpr.edu.br>
2025-03-07 22:08:44 -0300

    Coloquei as variáveis de ambiente

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ from collections import defaultdict
 # Carrega as variáveis de ambiente, com o objetivo de esconder as credenciais
 load_dotenv()
 
-# Pega o código de API da .env
+# Pega a chave de API da .env
 LASTFM_API_KEY = os.getenv("LASTFM_API_KEY")
+
+# Define a conta que será puxada as estatísticas
 username = "amiseralk"
+
 start_timestamp = int((datetime.datetime.now() - datetime.timedelta(days=7)).timestamp())
 end_timestamp =  int(datetime.datetime.now().timestamp())
 
@@ -29,7 +32,8 @@ for track in tracks:
 # Ordena e seleciona o top 10 artistas (o parâmetro reserve faz com que retorne a lista do mais ouvido para o menos ouvido)
 top_artists = sorted(artist_counts.items(), key=lambda x: x[1], reverse=True)[:10]
 
+# Cria a sessão de autenticação com seu usuário e senha
 client = Client()
 client.login(os.getenv("BLUESKY_USERNAME"), os.getenv("BLUESKY_PASSWORD"))
-text = "Meus top 10 artistas da semana: " + ", ".join([artist for artist, _ in top_artists])  + "\n- Provido por ScrobbleToSky"
+text = "My top ten artists from the week: " + ", ".join([artist for artist, _ in top_artists])  + "\n\nPowered by: Scrobble To Sky #LastFM"
 client.send_post(text)
